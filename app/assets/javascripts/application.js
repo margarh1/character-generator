d4d6a831c3153a2afde1f64cb5f227853478a01e
margarh1 <ha.margaret1@gmail.com>
2017-01-26 07:38:50 -0800

    changed has background to comparison

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,7 +52,7 @@ $(document).on('turbolinks:load', function() {
     get_data('backgrounds');
     var old_bg = $('.character_background').text();
     // unchecks, removes, and re-enables skills
-    if (has_background) {
+    if (has_background == true) {
       for (old_skill of data[old_bg]['skills']) {
         $('input[name^="skills_' + old_skill + '"]').prop('checked', false);
         $('input[name^="skills_' + old_skill + '"]').prop('disabled', false);
